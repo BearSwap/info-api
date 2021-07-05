d753e28ea15387da58748a40d46f4a3ba63958d6
BearSwap Finance <82644941+BearSwap@users.noreply.github.com>
2021-07-04 23:57:39 -0700

    Update response.ts

--- utils/response.ts
+++ utils/response.ts
@@ -15,10 +15,22 @@ export function return500(res: VercelResponse, name: string, symbol: string, pri
   }
 }
 
-export function return400(res: VercelResponse, message = "Bad request"): VercelResponse {
-  return returnError(res, 400, message);
+export function return500(res: VercelResponse, name: string, symbol: string, price: string, price_BNB: string): VercelResponse {
+  return res.status(500).json({
+    tokens: {
+     name,                    // not necessarily included for BEP20 tokens
+     symbol,                  // not necessarily included for BEP20 tokens
+     price,                   // price denominated in USD
+     price_BNB,               // price denominated in BNB
+  }
 }
 
-export function return500(res: VercelResponse, error: Error): VercelResponse {
-  return returnError(res, 500, error.message);
+export function return500(res: VercelResponse, name: string, symbol: string, price: string, price_BNB: string): VercelResponse {
+  return res.status(500).json({
+    tokens: {
+     name,                    // not necessarily included for BEP20 tokens
+     symbol,                  // not necessarily included for BEP20 tokens
+     price,                   // price denominated in USD
+     price_BNB,               // price denominated in BNB
+  }
 }
